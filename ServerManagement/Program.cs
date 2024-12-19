f477107c89d0787423741d70e498e660f81c062b
Emre Demirel <73110985+98emre@users.noreply.github.com>
2024-12-20 00:14:01 +0100

    feat: Container storage

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -1,10 +1,14 @@
 using ServerManagement.Components;
+using ServerManagement.StateStore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<SessionStorage>();
+builder.Services.AddScoped<ContainerStorage>();
 
 var app = builder.Build();
 
